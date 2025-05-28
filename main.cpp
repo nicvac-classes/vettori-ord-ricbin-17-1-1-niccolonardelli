ba38a62d5e58049f824a61b274884191d26eb597
niccolonardelli <niccolo.nardelli@marconibari.edu.it>
2025-05-28 15:21:38 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,62 @@
 #include <iostream>
+#include <vector>
+#include <ctime>
 using namespace std;
+int main() 
+{
+    srand(time(0));
+    int i, n, j, t, x;
+    float indice;
+    cout << "quanti numeri generare?" << endl;
+    cin >> n;
+    vector<int>r[n];
+    i=0;
+    while (i<n)
+    {
+        r[i]= rand()%101;
+        i=i+1;
+    }
+    i=0;
+    while (i<n)
+    {
+        j=0;
+        while (j<=n-2)
+        {
+            if (r[j]>r[j+1])
+            {
+                t=r[j];
+                r[j]= r[j+1];
+                r[j+1] = t;
+            }
+            j=j+1;
+        }
+        i=i+1;
+    }
+    i=0;
+    while(i<n)
+    {
+        cout << r[i] << endl;
+        i=i+1;
+    }
+    cout << "inserire un valore " << endl;
+    cin >> x;
+    i=0;
+    while (i<=n-1 and indice =-1)
+    {
+        if (r[i] = x)
+        {
+            indice =i;
+        }
+    }
+    if (indice =-1)
+    {
+        cout << "valore non trovato" << endl;
+    }
+    else
+    {
+        cout << "il valore " << x << " si trova nella cella " << indice << endl;
+    }
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
